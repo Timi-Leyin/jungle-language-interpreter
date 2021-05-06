b8820ce5f5f3d4c78ff3f2a060c2d30ba760354e
Timileyin <timileyinoyelekan11@gmail.com>
2021-05-06 17:53:42 +0000

    Converted to typescript

--- js/script.js
+++ js/script.js
@@ -1,99 +1,63 @@
-//declear varables
-const panelBtn=document.querySelectorAll('main .panels a'),
-panelTabs=document.querySelector('.panel-tabs'),
-      jungleInput=document.querySelector(' #home > div:first-child textarea');
-      jungleOutput=document.querySelector(' #home > div:last-child textarea'),
-          historyParentEl=document.querySelector('#history .history');
-
-let history,storage,saveTimer;
-history=[];
-storage=localStorage;
-    storage.setItem('history',JSON.stringify(history));
-
-class Jungle{
-  constructor(str){
-    this.str=str ;
-  }
-
-//convert words to jungle language
- convert(callback){
-    const { str }=this
-   let vowels=['a','e','i','o','u'].join("");
-   
- let vowelMatch="", err=null
-   if(str || str.match(/(a|e|i|o|u)/ig)){
-    vowelMatch=  str.replace(/a/ig,1).replace(/e/ig,2).replace(/i/ig,3).replace(/u/ig,5).replace(/o/ig,4)
-};
-let newArray=[];
-  let join;
-combine: for(let i=0; i< vowelMatch.length; i++){
-  join=newArray.push(vowelMatch[i])
-  if(!vowelMatch[i].match(/(a|e|i|o|u|[0-9]|\s|\W)/ig)){
-    join=newArray.push('a')
-
- // break combine;
-     //continue
-  }
-}
-  
-callback(err,[newArray.join(''), str])
-
-}
-
- reverse(callback){
-    const { str } =this;
-    let err=null,data=undefined;
-    callback(err,[str, data])
-    
-  }
-
-  }
-
+"use strict";
+//declear varables;
+var jungleInput = document.querySelector(" #home > .en textarea");
+var panelBtn = document.querySelectorAll("main .panels a");
+var panelTabs = document.querySelector(".panel-tabs");
+var jungleOutput = document.querySelector(" #home .en textarea");
+var historyParentEl = document.querySelector("#history .history");
+//  history
+var histories = [], storage;
+storage = localStorage;
+storage.setItem("histories", JSON.stringify(histories));
+// JUNGLE LANGUAGE CLASS
+var Jungle = /** @class */ (function () {
+    function Jungle(str) {
+        //  STRING TO BE CONVERTED
+        this.str = str;
+    }
+    //convert words to jungle language
+    Jungle.prototype.convert = function (callback) {
+        //  destructure string to be converted 
+        var str = this.str;
+        var vowels;
+        vowels = ["a", "e", "i", "o", "u"].join("");
+        var vowelMatch = '', err = null;
+        if (str || str.match(/(a|e|i|o|u)/gi)) {
+            vowelMatch = str
+                .replace(/a/gi, '1')
+                .replace(/e/gi, '2')
+                .replace(/i/gi, '3')
+                .replace(/o/gi, '4')
+                .replace(/u/gi, '5');
+        }
+        var newArray = [];
+        var join;
+        for (var i = 0; i < vowelMatch.length; i++) {
+            join = newArray.push(vowelMatch[i]);
+            if (!vowelMatch[i].match(/(a|e|i|o|u|[0-9]|\s|\W)/gi)) {
+                join = newArray.push("a");
+            }
+        }
+        callback(err, [newArray.join(""), str]);
+        return newArray.join('');
+    };
+    Jungle.prototype.reverse = function (callback) {
+        var str = this.str;
+        var err = null, data = undefined;
+        callback(err, [str, data]);
+    };
+    return Jungle;
+}());
 //update input field functions
-function save(val){
-    history.push({
-        val:val,
-        time:new Date().toLocaleTimeString(),
-        data:new Date().toLocaleDateString()
-    })
-        storage.setItem('history',JSON.stringify(history));
-let parseData =JSON.parse(storage.getItem('history'));
-//    console.log(parseData);
-    
-        console.clear()
-
-    parseData.forEach((value,index)=>{
-        const newHistoryEl =document.createElement('div');
-        newHistoryEl.setAttribute('class','history-card');
-        const node=document.createTextNode(value.val);
-       newHistoryEl.appendChild(node);
-        console.log(historyParentEl.append(newHistoryEl))
-console.log(value.val);
-        console.log(parseData)
-    })
-    return parseData;
-
+function update(arg) {
+    new Jungle(arg).convert(function (err, data) {
+        jungleOutput.value = data[0];
+    });
 }
-
-function update(arg){
-        new Jungle(arg).convert((err, data)=>{
-jungleOutput.value=data[0]
-    })
-
-}
-
-
-update(jungleInput.value)
-// save(jungleInput.value)
-
-jungleInput.addEventListener('input',(e)=>{
-    let {value}=e.target;
-    update(value);
-})
-
-jungleInput.addEventListener('blur',(e)=>{
- save(jungleInput.value)
-})
-
-
-   
+// update(jungleInput.value);
+jungleInput.addEventListener("input", function (e) {
+    update(e.target.value);
+});
+jungleInput.addEventListener("blur", function (e) {
+    //  save(jungleInput.value);
+});
--- js/script.ts
+++ js/script.ts
@@ -1,58 +1,56 @@
 
 //declear varables;
-let panelBtn, panelTabs, jungleInput, jungleOutput, historyParentEl ;
-panelBtn = document.querySelectorAll("main .panels a"),
-panelTabs = document.querySelector(".panel-tabs"),
-jungleInput = document.querySelector(" #home > .en textarea");
-(jungleOutput = document.querySelector(" #home > div:last-child textarea")),
- (historyParentEl = document.querySelector("#history .history"));
-
+let jungleInput:any = document.querySelector(" #home > .en textarea");
+let panelBtn = document.querySelectorAll("main .panels a");
+let panelTabs = document.querySelector(".panel-tabs");
+let jungleOutput:any = document.querySelector(" #home .en textarea");
+let historyParentEl = document.querySelector("#history .history");
 
 //  history
-let history, storage
-history = [];
+let histories:[]=[], storage
 storage = localStorage;
-storage.setItem("history", JSON.stringify(history));
+storage.setItem("histories", JSON.stringify(histories));
 
 
 // JUNGLE LANGUAGE CLASS
 class Jungle {
- constructor(str) {
+  str:string;
+ constructor(str:string) {
   //  STRING TO BE CONVERTED
   this.str = str;
  }
 
  //convert words to jungle language
- convert(callback) {
+ convert(callback:Function):string {
+  //  destructure string to be converted 
   const { str } = this;
-  let vowels = ["a", "e", "i", "o", "u"].join("");
+  let vowels : string;
+  vowels = ["a", "e", "i", "o", "u"].join("");
 
-  let vowelMatch = "",
+  let vowelMatch='',
    err = null;
   if (str || str.match(/(a|e|i|o|u)/gi)) {
    vowelMatch = str
-    .replace(/a/gi, 1)
-    .replace(/e/gi, 2)
-    .replace(/i/gi, 3)
-    .replace(/u/gi, 5)
-    .replace(/o/gi, 4);
+    .replace(/a/gi, '1')
+    .replace(/e/gi, '2')
+    .replace(/i/gi, '3')
+    .replace(/o/gi, '4')
+    .replace(/u/gi, '5');
   }
   let newArray = [];
   let join;
-  combine: for (let i = 0; i < vowelMatch.length; i++) {
+  for (let i = 0; i < vowelMatch.length; i++) {
    join = newArray.push(vowelMatch[i]);
    if (!vowelMatch[i].match(/(a|e|i|o|u|[0-9]|\s|\W)/gi)) {
     join = newArray.push("a");
-
-    // break combine;
-    //continue
    }
   }
 
   callback(err, [newArray.join(""), str]);
+  return newArray.join('')
  }
 
- reverse(callback) {
+ reverse(callback:Function) {
   const { str } = this;
   let err = null,
    data = undefined;
@@ -61,44 +59,18 @@ class Jungle {
 }
 
 //update input field functions
-function save(val) {
- history.push({
-  val: val,
-  time: new Date().toLocaleTimeString(),
-  data: new Date().toLocaleDateString(),
- });
- //         storage.setItem('history',JSON.stringify(history));
- // let parseData =JSON.parse(storage.getItem('history'));
- //    console.log(parseData);
-
- // console.clear()
-
- parseData.forEach((value, index) => {
-  const newHistoryEl = document.createElement("div");
-  newHistoryEl.setAttribute("class", "history-card");
-  const node = document.createTextNode(value.val);
-  newHistoryEl.appendChild(node);
-  console.log(historyParentEl.append(newHistoryEl));
-  console.log(value.val);
-  console.log(parseData);
- });
- return parseData;
-}
-
-function update(arg) {
- new Jungle(arg).convert((err, data) => {
+function update(arg:string) {
+ new Jungle(arg).convert((err:string, data:any[]) => {
   jungleOutput.value = data[0];
  });
 }
 
-update(jungleInput.value);
-// save(jungleInput.value)
+// update(jungleInput.value);
 
-jungleInput.addEventListener("input", (e) => {
- let { value } = e.target;
- update(value);
+jungleInput.addEventListener("input", (e:any) => {
+ update(e.target.value);
 });
 
-jungleInput.addEventListener("blur", (e) => {
- save(jungleInput.value);
+jungleInput.addEventListener("blur", (e:any) => {
+//  save(jungleInput.value);
 });
